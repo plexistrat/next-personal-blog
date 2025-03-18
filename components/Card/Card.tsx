0afc1a19d4e0c249733bc3eefbc0db4f15f4e56f
plexistrat <drososstaikos@yahoo.gr>
2025-03-18 13:22:47 +0200

    card passing props

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,10 +3,17 @@ import styles from './Card.module.css';
 
 function Card({ title, image, date }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardImage}>
-        <img src={image} alt="" />
-      </div>
+    <div className={styles.card} style={{ backgroundImage: `url(${image})` }}>
+      {/* <Image
+        loader={image}
+        // className={styles.cardImage}
+        width={400}
+        height={300}
+        src={image}
+        style={{ objectFit: 'contain' }}
+        alt="backround of the card"
+      /> */}
+
       <div className={styles.cardDummy}>
         <div className={styles.cardTitle}>
           <h2>{title}</h2>
